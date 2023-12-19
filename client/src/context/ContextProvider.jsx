67b489519b5d8d84928281602ed985f53d4e9d5f
Prabhashbhajani <prabhashbhajani@gmail.com>
2023-12-19 17:06:03 +0530

    user added to local storage

--- client/src/context/ContextProvider.jsx
+++ client/src/context/ContextProvider.jsx
@@ -1,11 +1,11 @@
-import React, { createContext, useContext, useReducer } from 'react'
+import React, { createContext, useContext, useEffect, useReducer } from 'react'
 import reducer from './reducer'
 
 const initailState = {
     currentUser: null,
     openLogin: false,
     loading: false,
-    alert: {open: false, severity: 'info', message: ''}
+    alert: { open: false, severity: 'info', message: '' }
 }
 
 const Context = createContext(initailState)
@@ -14,8 +14,15 @@ export const useValue = () => {
     return useContext(Context)
 }
 
-const ContextProvider = ({children}) => {
+const ContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initailState)
+
+    useEffect(() => {
+        const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        if (currentUser) {
+            dispatch({ type: 'UPDATE_USER', payload: currentUser });
+        }
+    }, []);
 
     return (
         <Context.Provider value={{ state, dispatch }}>
