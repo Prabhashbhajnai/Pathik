0a232d84dcac86b91a54a8e01f475b45e5ec5a28
utkarshMAVcode <utkarsh.mission@gmail.com>
2024-01-29 21:51:00 +0530

    store images and details in mongoDB and addroom access

--- client/src/components/BottomNav.jsx
+++ client/src/components/BottomNav.jsx
@@ -9,6 +9,7 @@ import {
   import ClusterMap from './map/ClusterMap';
   import Rooms from './rooms/Rooms';
   import AddRoom from './addRoom/AddRoom';
+import Protected from './protected/Protected';
   
   const BottomNav = () => {
     const [value, setValue] = useState(0);
@@ -22,7 +23,7 @@ import {
           {
             0: <ClusterMap />,
             1: <Rooms />,
-            2: <AddRoom />,
+            2: <Protected><AddRoom setPage={setValue}/></Protected>,
           }[value]
         }
         <Paper
